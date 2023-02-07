f556552bfc0c862215a5d9dfca5fe1735ac1c539
Romartiny <romartiny@Romartiny-2.local>
2023-02-07 21:13:16 +0300

    create post form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Counter from "./components/counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import postItem from "./components/PostItem";
+import PostForm from "./components/PostForm";
 
 function App() {
 
@@ -13,9 +17,24 @@ function App() {
         {id: 3, title: 'Test 3', body: 'Testing 3'},
     ]);
 
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const deletePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
     return (
         <div className="App">
-            <PostList posts={posts} title={'List of Posts'}/>
+            <PostForm create={createPost}/>
+            {posts.length !== 0
+                ? <PostList remove={deletePost} posts={posts} title={'List of Posts'}/>
+                : <h1 style={{textAlign: "center"}}>
+                    No posts
+                  </h1>
+            }
+
         </div>
     );
 }
