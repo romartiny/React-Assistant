11ea5b35cac702bc0ddc758ffe27bd753a554445
Romartiny <romartiny@Romartiny-2.local>
2023-02-07 16:37:04 +0300

    add styles and post components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, {useState} from "react";
 import Counter from "./components/counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
 
-    const [value, setValue] = useState('');
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Test 1', body: 'Testing 1'},
+        {id: 2, title: 'Test 2', body: 'Testing 2'},
+        {id: 3, title: 'Test 3', body: 'Testing 3'},
+    ]);
 
     return (
         <div className="App">
-            <Counter/>
-            <ClassCounter/>
+            <PostList posts={posts} title={'List of Posts'}/>
         </div>
     );
 }
