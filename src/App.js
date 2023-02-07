ce272fd104e0a42a33576d9327af15ea38ed3729
Romartiny <romartiny@Romartiny-2.local>
2023-02-07 13:47:55 +0300

    make simple inc and deinc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('');
+
+    function inc() {
+        setLikes(likes + 1);
+    }
+
+    function deinc() {
+        setLikes(likes - 1);
+    }
+
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input
+                type='text'
+                value={value}
+                onChange={event => setValue(event.target.value) }
+            />
+          <button onClick={inc}>Increment</button>
+          <button onClick={deinc}>Increment</button>
+        </div>
+    );
 }
 
 export default App;
