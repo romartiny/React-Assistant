d7fd4dafecdecca613448de03786a160cebbf343
Romartiny <romartiny@Romartiny-2.local>
2023-02-08 16:44:07 +0300

    create custom hook

--- src/App.js
+++ src/App.js
@@ -11,28 +11,19 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
-
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Test 1', body: 'Testing 1'},
-        {id: 2, title: 'Fest 2', body: 'Testing 2'},
-        {id: 3, title: 'Eest 3', body: 'Testing 3'},
-    ]);
+    //
+    // const [posts, setPosts] = useState([
+    //     {id: 1, title: 'Test 1', body: 'Testing 1'},
+    //     {id: 2, title: 'Fest 2', body: 'Testing 2'},
+    //     {id: 3, title: 'Eest 3', body: 'Testing 3'},
+    // ]);
 
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        return posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
